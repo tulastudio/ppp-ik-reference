aa0d8afebcfe20b64ee54c03dd418831e65c355f
Peter Simonsson <peter@simonsson.com>
2021-12-09 21:19:34 +0100

    Add simple error handling

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,42 @@
 const selectedList = document.querySelector<HTMLElement>('[data-selected]');
 const fundListEl = document.querySelector<HTMLElement>('[data-fund-list]');
 
-const fundList = window.initializeFundList(fundListEl, {
-	initialFunds: ['0P00000K9D', 'F0GBR05TYW'],
-	maxFunds: 3,
-});
-
-fundList.on('setFunds', (funds: string[]) => {
-	const nodes = funds.map(fund => {
-		const node = document.createElement('li');
-		const name = document.createTextNode(fund);
-		const button = document.createElement('button');
-		button.setAttribute('data-remove', fund);
-		button.textContent = '×';
-		node.appendChild(name);
-		node.appendChild(button);
-		return node;
+try {
+	const fundList = window.initializeFundList(fundListEl, {
+		initialFunds: ['0P00000K9D', 'F0GBR05TYW'],
+		maxFunds: 3,
 	});
 
-	while (selectedList.firstChild) {
-		selectedList.removeChild(selectedList.firstChild);
-	}
+	fundList.on('setFunds', (funds: string[]) => {
+		const nodes = funds.map((fund) => {
+			const node = document.createElement('li');
+			const name = document.createTextNode(fund);
+			const button = document.createElement('button');
+			button.setAttribute('data-remove', fund);
+			button.textContent = '×';
+			node.appendChild(name);
+			node.appendChild(button);
+			return node;
+		});
 
-	for (const node of nodes) {
-		selectedList.appendChild(node);
-	}
-});
+		while (selectedList.firstChild) {
+			selectedList.removeChild(selectedList.firstChild);
+		}
 
-selectedList.addEventListener('click', (event) => {
-	if (!(event.target instanceof HTMLElement)) return;
+		for (const node of nodes) {
+			selectedList.appendChild(node);
+		}
+	});
 
-	if (event.target.hasAttribute('data-remove')) {
-		event.stopPropagation();
-		const fundId = event.target.getAttribute('data-remove');
-		fundList.removeFund(fundId);
-	}
-});
+	selectedList.addEventListener('click', (event) => {
+		if (!(event.target instanceof HTMLElement)) return;
+
+		if (event.target.hasAttribute('data-remove')) {
+			event.stopPropagation();
+			const fundId = event.target.getAttribute('data-remove');
+			fundList.removeFund(fundId);
+		}
+	});
+} catch (error) {
+	fundListEl.innerHTML = 'Ett fel inträffade när listan skulle laddas.';
+}
